6cdc13748283acdffda718a88abdfd6b0615958f
AndrewKvash <94719715+AndrewKvash@users.noreply.github.com>
2022-03-11 18:32:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,26 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int l = 0;
+    int r = size - 1;
+    int res = 0;
+    while (l < r) {
+        int ser = (l + r) / 2;
+        if (arr[ser] < value) {
+            l = ser + 1;
+        }
+        else {
+            r = ser;
+        }
+    }
+    while (arr[l] == value) {
+        l++;
+        res++;
+    }
+    if (res == 0) {
+        return 0;
+    }
+    else {
+        return res;
+    }
 }
